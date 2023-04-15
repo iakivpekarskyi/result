0a4ecfb109ecd3a41cc2110fc9a04cef01cb4b3c
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-04-15 18:20:46 +0200

    4 done

---
+++ 4/app.js
@@ -0,0 +1,37 @@
+const board = document.querySelector("#board");
+const colors = [
+  "#9b5fe0",
+  "#16a4d8",
+  "#60dbe8",
+  "#8bd346",
+  "#efdf48",
+  "#f9a52c",
+  "#d64e12",
+];
+const SQUARE_NUMBER = 500;
+
+for (let i = 0; i < SQUARE_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
